151ec4c7f03693fc1f581aae9b010683a5834e81
yeeun <84173008+jye104@users.noreply.github.com>
2024-07-04 21:20:47 +0900

    add: main-하단 Snb 컨포넌트 퍼블리싱

---
+++ src/app/_components/main/Snb.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+
+import '../../../styles/global.css';
+
+const Snb = () => {
+  return (
+    <nav className="absolute bottom-0 left-0 w-full before:content-[''] before:w-full before:h-[130px] before:absolute before:bottom-0 before:left-0 before:bg-gradient-to-t from-white to-transparent">
+      <ul className="relative z-10 flex pt-[8px] pb-[24px] bg-white rounded-[20px_20px_0_0] shadow-[0_-4px_16px_0_#0000000D]">
+        <li className="w-[calc(100%_/_3)] flex flex-col">
+          <Link href={'/'} className="ico_pravel margin_center ico_home"></Link>
+          <span className="text-[11px]">홈</span>
+        </li>
+        <li className="w-[calc(100%_/_3)] flex flex-col">
+          <Link
+            href={''}
+            className="ico_pravel margin_center ico_records"
+          ></Link>
+          <span className="text-[11px]">여행기록</span>
+        </li>
+        <li className="w-[calc(100%_/_3)] flex flex-col">
+          <Link
+            href={''}
+            className="ico_pravel margin_center ico_mypage"
+          ></Link>
+          <span className="text-[11px]">마이페이지</span>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Snb;
