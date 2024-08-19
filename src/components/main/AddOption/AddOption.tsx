791deac836df9ef164154cac260984f851a8bdf3
yeeun <84173008+jye104@users.noreply.github.com>
2024-08-19 14:54:01 +0900

    update: useModal 훅 추가 및 적용

--- src/components/main/AddOption/AddOption.tsx
+++ src/components/main/AddOption/AddOption.tsx
@@ -1,31 +1,17 @@
 import Link from 'next/link';
 
-interface ModalOpenType {
-  openModalAddOption: boolean;
-  setOpenModalAddOption: React.Dispatch<React.SetStateAction<boolean>>;
-  openModalWishList: boolean;
-  setOpenModalWishList: React.Dispatch<React.SetStateAction<boolean>>;
+interface AddOptionProps {
+  closeAddOption: () => void;
+  openWishList: () => void;
 }
-const AddOption = ({
-  openModalAddOption,
-  setOpenModalAddOption,
-  openModalWishList,
-  setOpenModalWishList,
-}: ModalOpenType) => {
-  const onOpenModal = () => {
-    setOpenModalAddOption(!openModalAddOption);
-  };
 
-  const onOpenWishList = () => {
-    setOpenModalWishList(!openModalWishList);
-  };
-
+const AddOption = ({ closeAddOption, openWishList }: AddOptionProps) => {
   return (
     <div
-      className={`absolute bottom-0 left-0 w-full mb-[28px] pt-[10px] pb-[41px] z-10 bg-white animate-modalUp`}
+      className={`modal-content absolute bottom-0 left-0 w-full mb-[28px] pt-[10px] pb-[41px] z-10 bg-white animate-modalUp modal`}
     >
       <button
-        onClick={onOpenModal}
+        onClick={closeAddOption}
         className="blind block w-full before:content-[''] before:block before:w-[64px] before:h-[4px] before:bg-gray-300 before:mb-[28px] before:mx-[auto]"
       >
         하단으로 모달창 내리기
@@ -33,13 +19,11 @@ const AddOption = ({
       <div>
         <div className="flex px-[40px] gap-[14px] items-stretch">
           <button
-            onClick={onOpenWishList}
+            onClick={openWishList}
             className="relative w-1/2 bg-gray-100 border border-gray-300 rounded-[20px] pt-[30px] pb-[24px] before:content-[''] before:block before:w-[56px] before:h-[56px] before:bg-[url('/img_pravel.png')] before:bg-[-112px_-128px] before:bg-[length:300px_300px] before:m-[0_auto]"
           >
             <p className="absolute w-full top-[10px] px-[12px] flex justify-between">
-              <span className="blind w-[25px] h-[9px] bg-[url('/img_pravel.png')] bg-[-224px_-175px] bg-[length:300px_300px]">
-                New
-              </span>
+              <span className="ico_pravel ico_new25 my-[8px]">New</span>
               <span className="text-primary">3</span>
             </p>
             <span className="block pt-[19px]">나의 위시리스트</span>
---
+++ src/hook/useModal.ts
@@ -0,0 +1,74 @@
+import { useEffect, useState } from 'react';
+
+interface ModalState {
+  [key: string]: boolean;
+}
+
+type ModalActions = {
+  openModal: (modalName: string) => void;
+  closeModal: (modalName: string) => void;
+  toggleModal: (modalName: string) => void;
+  closeAllModals: () => void;
+};
+
+const useModal = (initState: ModalState = {}): [ModalState, ModalActions] => {
+  const [modalState, setModalState] = useState<ModalState>(initState);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    const target = event.target as HTMLElement;
+
+    if (!target.closest('.modal-content')) {
+      closeAllModals();
+    }
+  };
+
+  const openModal = (modal: string) => {
+    setModalState((prevState) => ({ ...prevState, [modal]: true }));
+
+    // 모달을 열 때 클릭 외부 이벤트 리스너를 등록
+    document.addEventListener('click', handleClickOutside);
+  };
+
+  const closeModal = (modal: string) => {
+    setModalState((prevState) => ({ ...prevState, [modal]: false }));
+
+    // 모든 모달이 닫히면 클릭 외부 이벤트 리스너를 제거
+    if (!Object.values(modalState).some((isOpen) => isOpen)) {
+      document.removeEventListener('click', handleClickOutside);
+    }
+  };
+
+  const toggleModal = (modal: string) => {
+    const isOpen = modalState[modal];
+
+    setModalState((prevState) => ({
+      ...prevState,
+      [modal]: !isOpen,
+    }));
+
+    // 모달을 토글할 때 상태에 따라 이벤트 리스너 등록/제거
+    if (!isOpen) {
+      document.addEventListener('click', handleClickOutside);
+    } else if (!Object.values(modalState).some((open) => open)) {
+      document.removeEventListener('click', handleClickOutside);
+    }
+  };
+
+  const closeAllModals = () => {
+    setModalState((prevState) => {
+      const newState = { ...prevState };
+
+      Object.keys(newState).forEach((modal) => {
+        newState[modal] = false;
+      });
+      return newState;
+    });
+
+    // 모든 모달을 닫으므로 이벤트 리스너 제거
+    document.removeEventListener('click', handleClickOutside);
+  };
+
+  return [modalState, { openModal, closeModal, toggleModal, closeAllModals }];
+};
+
+export default useModal;
