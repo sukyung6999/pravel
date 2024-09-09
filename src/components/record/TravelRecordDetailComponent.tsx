64a96e46cac99ea2a2b0c92ef80b409670ea78cb
suld2495 <suld7777@gmail.com>
2024-09-09 22:21:30 +0900

    fix: 여행 기록 리스트 페이지 뒤에 모달이 배치되는 문제 수정

--- src/components/record/TravelRecordDetailComponent.tsx
+++ src/components/record/TravelRecordDetailComponent.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
-import Link from 'next/link';
 
 import { Plan } from '@/types/plan.type';
+import BackButton from '../common/back/BackButton';
 
 interface TravelRecordDetailProps extends Plan {}
 
@@ -12,11 +12,9 @@ const TravelRecordDetailComponent = ({
   endDate,
 }: TravelRecordDetailProps) => {
   return (
-    <div className="pt-10 px-4">
+    <div className="pt-10 px-4 absolute top-0 bg-gray-100 w-full h-full min-h-dvh z-10">
       <div>
-        <Link className="absolute" href="/travel-record">
-          <i className="ico_pravel ico_left_black_arrow24" />
-        </Link>
+        <BackButton />
         <h1 className="font-semibold text-[18px] text-gray-900 flex items-center justify-center">
           <i className="ico_pravel ico_black_marker24" />
           {title}
