688be865209ffdbb136facafeb9e492d26416c45
suld2495 <suld7777@gmail.com>
2024-09-17 10:13:45 +0900

    feat: 리워드 이미지 교체

--- src/components/mypage/main/RewardList.tsx
+++ src/components/mypage/main/RewardList.tsx
@@ -8,7 +8,7 @@ const RewardList = ({
   totalRewardCount,
 }: RewardListProps) => {
   return (
-    <div className="mt-[10px] p-5 bg-white rounded-xl bg-[url('/mypage/trophy.png')] bg-no-repeat bg-right">
+    <div className="mt-[10px] p-5 bg-white rounded-xl bg-[url('/mypage/trophy.png')] bg-no-repeat bg-contain bg-right">
       <div className="flex justify-between items-center text-[13px] w-[165px]">
         <span className="text-gray-600">획득한 리워드</span>
         <div>
@@ -22,7 +22,7 @@ const RewardList = ({
         <div className="container h-full bg-gray-100 rounded-[15px]" />
         <div className="absolute w-[60%] top-0 value h-full bg-primary rounded-[15px]" />
       </div>
-      <div className="w-[calc(100%-159px)] flex gap-[5px] mt-[10px]">
+      <div className="w-[calc(100%-159px)] flex gap-[5px] mt-[16px]">
         <div className="flex-1 h-[54px] bg-gray-100 rounded-lg py-[6px] pl-3 pr-[19px]">
           <h3 className="font-semibold text-[13px] text-gray-600">쿠폰</h3>
           <div className="flex items-center justify-between">
