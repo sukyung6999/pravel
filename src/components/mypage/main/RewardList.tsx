4e91a207dc14e06f666e817c98392d9b07322a16
suld2495 <suld7777@gmail.com>
2024-09-22 13:19:55 +0900

    update: 리워드 반응형 작업

--- src/components/mypage/main/RewardList.tsx
+++ src/components/mypage/main/RewardList.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
+
 import { MypageTotalCountResponse } from '@/types/mypage.type';
 
 type RewardListProps = Omit<MypageTotalCountResponse, 'currentEnergyCount'>;
@@ -7,9 +11,16 @@ const RewardList = ({
   currentGiftCount,
   totalRewardCount,
 }: RewardListProps) => {
+  const percentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!percentRef.current) return;
+    percentRef.current.style.width = `${(1 / totalRewardCount) * 100}%`;
+  }, [percentRef, currentCouponCount, currentGiftCount, totalRewardCount]);
+
   return (
     <div className="mt-[10px] p-[20px] bg-white rounded-[12px] bg-[url('/mypage/trophy.png')] bg-no-repeat bg-contain bg-right">
-      <div className="flex justify-between items-center text-[13px] w-[165px]">
+      <div className="flex justify-between items-center text-[13px] min-w-[165px] w-[calc(100%-159px)]">
         <span className="text-gray-600">획득한 리워드</span>
         <div>
           <span className="text-primary">
@@ -18,9 +29,12 @@ const RewardList = ({
           <span className="text-gray-400">/{totalRewardCount}</span>
         </div>
       </div>
-      <div className="relative mt-[10px] w-[165px] h-[5px]">
+      <div className="relative mt-[10px] min-w-[165px] h-[5px] w-[calc(100%-159px)]">
         <div className="container h-full bg-gray-100 rounded-[15px]" />
-        <div className="absolute w-[60%] top-0 value h-full bg-primary rounded-[15px]" />
+        <div
+          ref={percentRef}
+          className={`absolute top-0 w-0 value h-full bg-primary rounded-[15px] transition-all`}
+        />
       </div>
       <div className="w-[calc(100%-159px)] flex gap-[5px] mt-[16px]">
         <div className="flex-1 h-[54px] bg-gray-100 rounded-[8px] py-[6px] pl-[12px] pr-[19px]">
