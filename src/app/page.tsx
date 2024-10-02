2df3ff23adef56c39144eff890dfdf0d4ce621bb
yeeun <84173008+jye104@users.noreply.github.com>
2024-10-02 11:15:25 +0900

    modify: 클라이언트, 서버 컴포넌트 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,11 @@
-import WishList from '@/components/main/AddOption/WishList';
+import MainModalWrapper from '@/components/main/MainModalWrapper';
 import ScheduleList from '@/components/main/Schedule/ScheduleList';
-import useModal, { MODAL } from '@/hook/useModal';
 import Header from '@/layout/header/Header';
 
-import AddOption from '../components/main/AddOption/AddOption';
 import DateViewer from '../components/main/DateHeader';
-import FloatingBar from '../components/main/FloatingBar';
 import Gnb from '../layout/navigation/Gnb';
 
 const Home = () => {
-  const [modalState, { openModal, closeModal }] = useModal({
-    [MODAL.ADD_OPTION]: false,
-    [MODAL.WISH_LIST]: false,
-  });
-
   return (
     <>
       <main className="relative min-h-dvh pb-[134px] bg-gray-100">
@@ -23,25 +15,9 @@ const Home = () => {
           <DateViewer />
           <ScheduleList />
         </>
-        <FloatingBar openAddOption={() => openModal(MODAL.ADD_OPTION)} />
+
+        <MainModalWrapper />
       </main>
-      {modalState.addOption && (
-        <AddOption
-          closeAddOption={() => closeModal(MODAL.ADD_OPTION)}
-          openWishList={() => openModal(MODAL.WISH_LIST)}
-        />
-      )}
-      {modalState.wishList && (
-        <WishList
-          closeWishList={() => {
-            closeModal(MODAL.WISH_LIST);
-          }}
-          closeModals={() => {
-            closeModal(MODAL.WISH_LIST);
-            closeModal(MODAL.ADD_OPTION);
-          }}
-        />
-      )}
     </>
   );
 };
